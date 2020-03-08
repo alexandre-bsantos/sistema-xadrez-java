6d043e935ab5d020182b1926f92783cda8cfb62d
Alexandre dos Santos <alexandre_bsantos12@hotmail.com>
2020-03-08 19:26:37 -0300

    Trocando de jogador a cada turno

--- src/application/Xadrez.java
+++ src/application/Xadrez.java
@@ -20,7 +20,7 @@ public class Xadrez {
 			try {
 				UI.clearScreen();
 				// UI = User interface (Inteface do usuário) - Classe
-				UI.printBoard(chessMatch.getPieces());
+				UI.printMatch(chessMatch);
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
